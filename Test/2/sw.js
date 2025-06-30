124794b0b5c099a13fcda396e26a4ffc98357c82
muif <162582545+muif@users.noreply.github.com>
2025-06-30 23:39:24 +0300

    Update sw.js

--- Test/2/sw.js
+++ Test/2/sw.js
@@ -1,13 +1,13 @@
 // sw.js
 const CACHE_NAME = 'internet-manager-v2';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png',
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js',
     'https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js'
